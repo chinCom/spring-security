f039a307d127424eeca0e5ca75aa814fd62547d5
Chin Com <duchinhpro@gmail.com>
2023-05-05 08:57:21 +0700

    spring-security

---
+++ src/main/java/com/security/springsecurity/webconfig/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.security.springsecurity.webconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        /*
+         * The SecurityFilterChain bean defines which URL paths should be secured and
+         * which should not. Specifically, the / and /home paths are configured to not
+         * require any authentication. All other paths must be authenticated.
+         */
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/", "home").permitAll().anyRequest().authenticated()
+
+        ).formLogin((form) -> form.loginPage("/login").permitAll()).logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+
+    // set user name and pasword
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
